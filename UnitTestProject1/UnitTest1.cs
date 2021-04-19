8054eeb4440dad3e4eba3e30227e4150397ef253
Lin, Chia-Hsuan <cheriehsuan@gmail.com>
2021-04-20 01:41:04 +0800

    test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using practice_0004.Class;
+using Xunit;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrage
+            var EmployeeClass = new EmployeeClass();
+
+            //Act
+            var Result = EmployeeClass.Emp_idIsExist("test22223");
+
+            //Assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(Result);
+        }
+
+        //[Theory]
+        //[InlineData("test22223", true)]
+        //[InlineData("test22223", false)]
+        //public void TestMethod2(string Emp_id, bool Result)
+        //{
+        //    //Arrage
+        //    var EmployeeClass = new EmployeeClass();
+
+        //    //Act
+        //    var test = EmployeeClass.Emp_idIsExist(Emp_id);
+
+        //    //Assert
+        //    if (Result)
+        //        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(test);
+        //    else
+        //        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(test);
+        //}
+    }
+}
